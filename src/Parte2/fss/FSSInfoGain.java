78856a2d8ab898e26d85c2cd5e016b1629dfab56
DavidPerezGomez <davidperezgo@gmail.com>
2018-04-24 13:14:33 +0200

    arreglado fss para hacer dev compatible

--- src/Parte2/fss/FSSInfoGain.java
+++ src/Parte2/fss/FSSInfoGain.java
@@ -1,8 +1,7 @@
 package Parte2.fss;
 
 
-import java.io.File;
-
+import utils.Utils;
 import weka.attributeSelection.InfoGainAttributeEval;
 import weka.attributeSelection.Ranker;
 import weka.core.Instances;
@@ -10,37 +9,50 @@ import weka.core.converters.ArffSaver;
 import weka.filters.Filter;
 import weka.filters.supervised.attribute.AttributeSelection;
 
+import java.io.File;
+
 public class FSSInfoGain {
+
     public static void main(String[] args) {
-        String trainBow = null;
-        String trainBowFSS=null;
+        String inputArff = null;
+        String outputArff = null;
+        String inputDev, outputDev;
         try {
-        	trainBow = args[0];
-        	trainBowFSS=args[1];
-        	
+        	inputArff = args[0];
+        	outputArff = args[1];
         } catch (IndexOutOfBoundsException e) {
 			 String documentacion = "Este ejecutable se utiliza para descartar atributos redudantes o irrelevantes para el proceso de clasificacion\n" +
-                                    "El archivo .arff original debe tener por lo menos un atributo de tipo string.\n" +
-                                    "Dos argumentos esperados:\n" +
-                                    "\t1 - Ruta del archivo .arff a leer\n" +
-                                    "\t2 - Ruta del archivo .arff a crear\n"+
-                                    "\nEjemplo: java -jar FSS.jar /path/to/input/arff /path/to/output/arff";
+                                            "El archivo .arff original debe tener por lo menos un atributo de tipo string.\n" +
+                                            "Dos argumentos esperados:\n" +
+                                            "\t1 - Ruta del archivo .arff a leer (la clase debe ser el primer atributo)\n" +
+                                            "\t2 - Ruta del archivo .arff a crear\n"+
+                                            "Argumentos opcionales:\n"+
+                                            "\t3 - Ruta del archivo .arff que se quiere hacer compatible con el .arff filtrado\n"+
+                                            "\t4 - Ruta del archivo .arff compatibilizado a crear\n"+
+                                            "\nEjemplo1: java -jar FSS.jar /path/to/input/arff /path/to/output/arff"+
+                                            "\nEjemplo2: java -jar FSS.jar /path/to/input/arff /path/to/output/arff /path/to/other/input/arff /path/to/other/output/arff";
             System.out.println(documentacion);
             System.exit(1);
-        } catch (IllegalArgumentException e) {
-            utils.Utils.printlnWarning(String.format("Argumento %s incorrecto."));
-            System.exit(1);
+        }
+
+        try {
+            inputDev = args[2];
+            outputDev = args[3];
+        } catch (IndexOutOfBoundsException e) {
+            inputDev = null;
+            outputDev = null;
         }
         //cargamos el fichero
-        Instances datos=utils.Utils.loadInstances(trainBow,0); 
+        Instances instances = utils.Utils.loadInstances(inputArff,0);
         try {
-			Instances filtrado=useFilter(datos);
-			
-			ArffSaver saver = new ArffSaver();
-	        saver.setInstances(filtrado);
-	        saver.setFile(new File(trainBowFSS));
-	        saver.writeBatch();
-			
+            AttributeSelection filter = getASFilter(instances);
+			Instances filteredInstances = Filter.useFilter(instances, filter);
+            Utils.saveInstances(filteredInstances, outputArff);
+            if (inputDev != null) {
+                Instances dev = utils.Utils.loadInstances(inputDev);
+                Instances filteredDev = Filter.useFilter(dev, filter);
+                Utils.saveInstances(filteredDev, outputDev);
+            }
 		} catch (Exception e) {
 			System.out.println("No se ha podido completar el filtrado ");
 			e.printStackTrace();
@@ -54,34 +66,29 @@ public class FSSInfoGain {
      * teniendo en cuenta que parara la primera vez que disminuyan los atributos. 
      * @param pDatos
      */
-    public static Instances useFilter(Instances pDatos) throws Exception {
-		AttributeSelection as=new AttributeSelection();
-		Ranker r=new Ranker();
-		
-		boolean salir=false;
-		double i=-0.005; 
-		int numAtributos=pDatos.numAttributes();
-		Instances filtrado=null;
+    private static AttributeSelection getASFilter(Instances pDatos) throws Exception {
+		AttributeSelection filter = new AttributeSelection();
+		Ranker ranker = new Ranker();
+		boolean salir = false;
+		double threshold = -0.001;
+		double increment = 0.0005;
+		int numAtributos = pDatos.numAttributes();
+		Instances filtrado;
+		Instances inputFormat = new Instances(pDatos, 0, 1);
+
 		while(!salir) {
-			r.setThreshold(i);
-			as.setEvaluator(new InfoGainAttributeEval());
-			as.setSearch(r);
-			as.setInputFormat(pDatos);
-			filtrado=Filter.useFilter(pDatos, as);
-			if(numAtributos!=filtrado.numAttributes()) {
-				salir=true;
-			}
-			i=i+0.0005;
-			System.out.println(i);
+            filter.setInputFormat(inputFormat);
+            filter.setEvaluator(new InfoGainAttributeEval());
+			ranker.setThreshold(threshold);
+            filter.setSearch(ranker);
+			filtrado = Filter.useFilter(pDatos, filter);
+			if(numAtributos != filtrado.numAttributes()) {
+				salir = true;
+			} else {
+                threshold += increment;
+            }
 		}
-		System.out.println("==========================================================");
-		System.out.println("NUMTOSELECT: POR DEFECTO (-1)");
-		System.out.println("THRESHOLD UTILIZADO: "+i);
-    	System.out.println("==========================================================");
-    	System.out.println("");
-    	System.out.println("");
-    	System.out.println("");
-		return filtrado;	
+		return filter;
     }
    
 }
